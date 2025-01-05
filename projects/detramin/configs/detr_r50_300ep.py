096a9d6794f7fdee370d2295651cf0d3a053dabf
hesum.shani <hesum.shani@gmail.com>
2025-01-06 02:44:24 +0330

    revert to batch size 2

--- projects/detramin/configs/detr_r50_300ep.py
+++ projects/detramin/configs/detr_r50_300ep.py
@@ -24,4 +24,4 @@ optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in modul
 
 # modify dataloader config
 dataloader.train.num_workers = 1
-dataloader.train.total_batch_size = 1
+dataloader.train.total_batch_size = 2
